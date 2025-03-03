f1c730374a71375c21eea74d8cdcf6b909f191f9
Nitin Singh <n55567274@gmail.com>
2025-03-03 22:17:40 +0530

    First Comit

---
+++ app.js
@@ -0,0 +1,14 @@
+const button="Button Added";
+console.log(button);
+
+console.log ("Login Page added");
+
+console.log("footer Added");
+console.log("Hello");
+
+console.log("Adding to master after payment commitment");
+console.log("Yes Commited");
+console.log("Hello Adding Payment Methid");
+
+const upi="Adding upi";
+console.log(upi);
